a78e91dcfd30172a702f21170ef6b9fc1587b731
rcartee <rcartee@outlook.com>
2025-05-29 16:20:33 -0400

    Create complex.java

---
+++ complex.java
@@ -0,0 +1,21 @@
+public class Complex
+{
+  private int real;
+  private int img;
+
+  public Complex(int r, int i)
+  {
+    real = r;
+    img = i;
+  }
+
+  int getReal()
+  {
+    return real;
+  }
+
+  int getImg()
+  {
+    return img;
+  }
+}
